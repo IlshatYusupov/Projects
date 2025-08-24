94f1d05ffbdfeab6155f63313fe66dac9bbe9835
IlshatYusupov <yusupovilshatilus@gmail.com>
2025-08-24 22:24:55 +0300

    Create PracticalWork3.2.cpp
    Задача 2. Обмен местами

---
+++ PracticalWork3.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a = 42;
+    int b = 153;
+    int c;
+    std::cout << "a: " << a << "\n";      /* На экран будет выведено 42 */
+    std::cout << "b: " << b << "\n";      /* На экран будет выведено 153 */
+
+    c = a;
+    a = b;
+    b = c;
+
+    std::cout << "a: " << a << "\n";      /* На экран будет выведено 153 */
+    std::cout << "b: " << b << "\n";      /* На экран будет выведено 42 */
+}

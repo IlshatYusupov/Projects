c01f6517ff8b49339a0b85ed623591863b76695d
Ilshat <yusupov_ilshat@mail.ru>
2025-09-07 10:36:59 +0300

    Create PracticalWork4.4.cpp

---
+++ PracticalWork4.4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    
+    std::cout << "Введите число очков опыта: ";
+    std::cin >> x;
+
+    std:: cout << "---------Считаем-------- \n";
+
+    
+
+    if (x >= 5000) {
+      std::cout << "Ваш уровень:" << 4 << "\n";
+  }   
+    else if (x >= 2500) {
+      std::cout << "Ваш уровень:" << 3 << "\n";
+  }
+    else if (x >= 1000) {
+    std::cout << "Ваш уровень:" << 2 << "\n";
+  }
+    else if (x >=0) {
+    std::cout << "Ваш уровень:" << 1 << "\n";
+  }
+    else   {  
+    std::cout << "Ведите положительное число \n";
+  }
+}

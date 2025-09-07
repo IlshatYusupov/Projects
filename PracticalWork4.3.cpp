6741613144474f2c1598948c50adee52c5cd1a0c
Ilshat <yusupov_ilshat@mail.ru>
2025-09-07 10:36:55 +0300

    Create PracticalWork4.3.cpp

---
+++ PracticalWork4.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    int remainder;
+    
+    std::cout << "Введите число: ";
+    std::cin >> x;
+
+    std:: cout << "---------Проверяем-------- \n";
+
+    remainder = x % 2;
+
+    if (remainder == 0) {
+    std::cout << "Число " << x << " — чётное \n";
+}   
+    else {
+    std::cout << "Число " << x << " — не чётное \n";
+}
+}

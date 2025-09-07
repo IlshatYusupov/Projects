8cdabf4038866ce935471ce06efbfb77066d8967
Ilshat <yusupov_ilshat@mail.ru>
2025-09-07 10:36:48 +0300

    Create PracticalWork4.2.cpp

---
+++ PracticalWork4.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    int y;
+    int z;
+    int sum;
+    
+    std::cout << "Введите первое число: ";
+    std::cin >> x;
+    std::cout << "Введите второе число: ";
+    std::cin >> y;
+    std::cout << "Введите их сумму: ";
+    std::cin >> sum;
+    std:: cout << "---------Проверяем-------- \n";
+
+    z = x + y;
+
+    if (sum == z) {
+        std::cout << "Верно! \n";
+    }
+    if (sum != z) { 
+        std::cout << "Ошибка! Верный результат: " << z << "\n";
+    }
+}

1b2926da661532674d0e31e370c416cca2082db8
Ilshat <yusupov_ilshat@mail.ru>
2025-09-07 10:37:02 +0300

    Create PracticalWork4.5.cpp

---
+++ PracticalWork4.5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    int y;
+    
+    std::cout << "Введите первое число: ";
+    std::cin >> x;
+    std::cout << "Введите второе число: ";
+    std::cin >> y;
+
+    std:: cout << "-----Проверяем-----  \n";
+
+    int remainder = x % y;
+
+    if (remainder == 0) {
+      std::cout << " Да " << x << " делится на " << y << " без остатка! \n";
+  }   
+    else {
+      std::cout << " Нет " << x << " не делится на " << y << " без остатка! \n";
+  }
+}

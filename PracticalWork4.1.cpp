601038be036259c573395ef162685667c313cb33
Ilshat <yusupov_ilshat@mail.ru>
2025-09-07 10:36:42 +0300

    Create PracticalWork4.1.cpp

---
+++ PracticalWork4.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int x;
+    int y;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> x;
+    std::cout << "Введите второе число: ";
+    std::cin >> y;
+    std:: cout << "---------Проверяем-------- \n";
+
+    if (x > y) {
+        std::cout << "Наименьшее число: " << y << "\n";
+    }
+    if (x < y) { 
+        std::cout << "Наименьшее число: " << x << "\n";
+    } 
+    else if (x==y) {
+        std::cout << " Числа равны! \n";
+}
+}
